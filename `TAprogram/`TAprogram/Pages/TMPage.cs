5cc298c38a560914783a1ca3ad40e1b2a070d530
Äravind Ramesh <aravind.ramesh@toyota.com.au>
2024-07-19 14:12:56 +1000

    SpecFlow Implementation edit <code>

--- `TAprogram/`TAprogram/Pages/TMPage.cs
+++ `TAprogram/`TAprogram/Pages/TMPage.cs
@@ -41,12 +41,12 @@ namespace TAprogram.Pages
 
             //enter text on codeTextbox
             IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
-            codeTextbox.SendKeys("TA Program");
+            codeTextbox.SendKeys("Industry Connect");
 
 
             //enter text on descriptionTextbox
             IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
-            descriptionTextbox.SendKeys("Test Analyst");
+            descriptionTextbox.SendKeys("TA");
 
 
 
@@ -70,79 +70,45 @@ namespace TAprogram.Pages
             assertionButton.Click();
             Thread.Sleep(2000);
 
-            IWebElement newcode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
-
-            //Instead of Console.Writeline we are using assertion
-            Assert.That(newcode.Text == "TA Program", "Time record has not created successfully");
-            /*if (newcode.Text == "TA Program")
-            {
-                Assert.Pass("Time record created successfully");
-
-            }
-            else
-            {
-                Assert.Fail("Time record has not been created");
-            }*/
 
         }
-        public void EditTimeRecord(IWebDriver driver)
+
+        public string GetCode(IWebDriver driver)
+        {
+            IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+            return newCode.Text;
+        }
+        public string GetDescription(IWebDriver driver)
+        {
+            IWebElement newDescription = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[3]"));
+            return newDescription.Text;
+        }
+        public string GetPrice(IWebDriver driver)
+        {
+            IWebElement newPrice = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[4]"));
+            //Console.WriteLine("######" + newPrice.Text);
+            return newPrice.Text;
+        }
+        public void EditTimeRecord(IWebDriver driver, string code)
         {
             Thread.Sleep(4000);
 
             //button going to the last page
-            IWebElement pageLastButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]"));
+            IWebElement pageLastButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[last()]/a[4]"));
             Thread.Sleep(2000);
             pageLastButton.Click();
             Thread.Sleep(2000);
 
             //click the button EDIT
             IWebElement editButton = driver.FindElement(By.XPath("//tr[last()]/td[last()]/a[1]"));
-            Thread.Sleep(2000);
             editButton.Click();
-            Thread.Sleep(2000);
-
-
-            //edit or change dropdown from Time to Material
-            IWebElement timeAndMDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]"));
-            timeAndMDropdown.Click();
-            Thread.Sleep(2000);
-
-
-            IWebElement editToTimeDropdown = driver.FindElement(By.XPath("//*[@id=\"TypeCode_option_selected\"]"));
-            editToTimeDropdown.Click();
-            Thread.Sleep(2000);
-
-            //edit or change code from TA Program to Program
+            
+            //edit or update code element
             IWebElement editCodeTextbox = driver.FindElement(By.Id("Code"));
             editCodeTextbox.Clear();
-            editCodeTextbox.SendKeys("ABC");
+            editCodeTextbox.SendKeys(code);
 
-
-            //edit or change description from Test Analyst to ANALYST
-            IWebElement editDescriptionTextbox = driver.FindElement(By.Id("Description"));
-            editDescriptionTextbox.Clear();
-            editDescriptionTextbox.SendKeys("SSS");
-            Thread.Sleep(2000);
-
-
-            IWebElement editPriceTextboxx = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
-            editPriceTextboxx.Click();
-            Thread.Sleep(2000);
-
-
-            //edit or change price from 20 to 6000
-            IWebElement editPriceTextbox = driver.FindElement(By.Id("Price"));
-            //editPriceTextboxx.Clear();
-
-            editPriceTextbox.SendKeys(Keys.Backspace);
-            editPriceTextbox.SendKeys(Keys.Backspace);
-            editPriceTextbox.SendKeys(Keys.Backspace);
-
-            editPriceTextbox.SendKeys("60");
-
-
-            Thread.Sleep(2000);
-
+           
             IWebElement editSaveButton = driver.FindElement(By.Id("SaveButton"));
             Thread.Sleep(1000);
             editSaveButton.Click();
@@ -155,15 +121,16 @@ namespace TAprogram.Pages
             Thread.Sleep(2000);
             pageLastButton1.Click();
             Thread.Sleep(2000);
-            
-
-            /*IWebElement neweditcode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
-            Thread.Sleep(2000);
-
-            //Instead of Console.Writeline we are using assertion
-            Assert.That(neweditcode.Text == "ABC", "Time record has not been edited");
-            */
         }
+
+        public string GetEditCode(IWebDriver driver)
+        {
+            IWebElement editcode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+            return editcode.Text;
+        }
+
+        //Assert.That(neweditcode.Text == "ABC", "Time record has not been edited");
+
         public static void DeleteTimeRecord(IWebDriver driver)
         {
             //click delete button
@@ -180,6 +147,6 @@ namespace TAprogram.Pages
             Console.WriteLine("Time record deleted");
         }
 
-
     }
+    
 }
--- `TAprogram/`TAprogram/StepDefination/TMfeatureStepDefinitions.cs
+++ `TAprogram/`TAprogram/StepDefination/TMfeatureStepDefinitions.cs
@@ -1,33 +1,81 @@
+using _TAprogram.Utilities;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
 using System;
+using TAprogram.Pages;
 using TechTalk.SpecFlow;
 
 namespace _TAprogram.StepDefination
 {
     [Binding]
-    public class TMfeatureStepDefinitions
+    public class TMfeatureStepDefinitions : CommonWebDriver
     {
         [Given(@"I logged into TurnUp portal successfully")]
         public void GivenILoggedIntoTurnUpPortalSuccessfully()
         {
-            throw new PendingStepException();
+            // open chrome browser
+            driver = new ChromeDriver();
+
+            //Login page object initialization and definations
+            TAprogram.Pages.LoginPage loginPageObj = new LoginPage();
+            loginPageObj.LoginActions(driver);
         }
 
         [When(@"I navigate to Time and Material page")]
         public void WhenINavigateToTimeAndMaterialPage()
         {
-            throw new PendingStepException();
+            //Home page object Initialization and definations
+            HomePage homePageobj = new HomePage();
+            homePageobj.NavigateToTMPage(driver);
         }
 
         [When(@"I create a time record")]
         public void WhenICreateATimeRecord()
         {
-            throw new PendingStepException();
+            //TM page object create initialization and definations
+            TMPage tmPageCreateobj = new TMPage();
+            tmPageCreateobj.CreateTimeRecord(driver);
         }
 
         [Then(@"the record should be created successfully")]
         public void ThenTheRecordShouldBeCreatedSuccessfully()
         {
-            throw new PendingStepException();
+            TMPage tmPageCreateobj = new TMPage();
+
+            string newCode = tmPageCreateobj.GetCode(driver);
+            string newDescription = tmPageCreateobj.GetDescription(driver);
+            string newPrice = tmPageCreateobj.GetPrice(driver);
+
+            Assert.That(newCode == "Industry Connect", "Actual Code and Expected Code do not match");
+            Assert.That(newDescription == "TA", "Actual Description and Expected Description do not match");
+            Assert.That(newPrice == "$200.00", "Actual Price and Expected Price do not match");
+
         }
+
+
+        //Edit records
+
+        [When(@"I update the '([^']*)'on an existing Time record")]
+        public void WhenIUpdateTheOnAnExistingTimeRecord(string code)
+        {
+            TMPage tMPageObj = new TMPage();
+            tMPageObj.EditTimeRecord(driver, code);
+        }
+
+        [Then(@"the record should have the updated '([^']*)'")]
+        public void ThenTheRecordShouldHaveTheUpdated(string code)
+
+        {
+            TMPage tMPageObj = new TMPage();
+
+            string editCode = tMPageObj.GetCode(driver);
+
+            Assert.That(editCode == code, "Actual and Expected Code do not match");
+
+
+        }
+
+
     }
 }
+
